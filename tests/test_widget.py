393e5267722159459f32113505c2ff11969dd34b
ne1ntrovert <shironovilya777@gmail.com>
2025-02-21 20:09:41 +0300

    я исправил все ошибки

--- tests/test_widget.py
+++ tests/test_widget.py
@@ -2,6 +2,7 @@ import pytest
 
 from src.widget import get_date, mask_account_card
 
+
 @pytest.mark.parametrize(
     "acc, expected",
     [
@@ -14,13 +15,22 @@ from src.widget import get_date, mask_account_card
 def test_mask_account_card(acc: str, expected: str) -> None:
     assert mask_account_card(acc) == expected
 
-    @pytest.mark.parametrize(
-        "date, expected",
-        [
-            ("2023-08-15", "15.08.23"),
-            ("2000-01-01", "01.01.00"),
-            ("2023-03-11T02:26:18.671407", "11.03.23"),
-        ],
-    )
-    def test_get_date(date: str, expected: str) -> None:
-        assert get_date(date) == expected
+
+@pytest.mark.parametrize("acc", ["", None, "gdfngdf gdf", "1234", "867524565463"])
+def test_mask_account_card_invalid(acc: str) -> None:
+    """проверка на typeerror"""
+    if acc is None:
+        with pytest.raises(TypeError):
+            mask_account_card(acc)
+
+
+@pytest.mark.parametrize(
+    "date, expected",
+    [
+        ("2023-08-15", "15.08.23"),
+        ("2000-01-01", "01.01.00"),
+        ("2024-03-11T02:26:18.671407", "11.03.24"),
+    ],
+)
+def test_get_date(date: str, expected: str) -> None:
+    assert get_date(date) == expected
