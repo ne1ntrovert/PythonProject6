45c81b6ef76e03f5dd9aa52e53ef5955aa811cd1
ne1ntrovert <shironovilya777@gmail.com>
2025-04-08 00:14:57 +0300

    добавил и изменил очень много

--- src/generators.py
+++ src/generators.py
@@ -15,13 +15,13 @@ file_handler.setFormatter(file_formatter)
 
 logger.addHandler(file_handler)
 
+
 def get_mask_card_number(card_number: str) -> str:
     """маскировка номера карты"""
     card_str = str(card_number)
     masked_card = f"{card_str[:4]} {card_str[4:6]}** **** {card_str[-4:]}"
     logger.info(f"Маскированный номер карты: {masked_card}")
     return masked_card
-
 
 
 def get_mask_account(account_number: str) -> str:
--- tests/test_decorators.py
+++ tests/test_decorators.py
@@ -1,5 +1,4 @@
 import os
-
 import pytest
 
 from src.decorators import log
